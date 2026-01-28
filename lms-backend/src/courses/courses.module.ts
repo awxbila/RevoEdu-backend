c96e14fb6572af41f8218380ae86ecf719d41900
Nabilah Nur Hazimah <nablahnur54@gmail.com>
2026-01-28 22:11:18 +0700

    6

--- lms-backend/src/courses/courses.module.ts
+++ lms-backend/src/courses/courses.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { S3Service } from '../common/s3.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [CoursesController],
-  providers: [CoursesService],
+  providers: [CoursesService, S3Service],
   exports: [CoursesService],
 })
 export class CoursesModule {}
